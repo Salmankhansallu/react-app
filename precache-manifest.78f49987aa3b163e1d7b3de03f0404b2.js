5fbea8d5ecfc1edc3aed983e17e9909a0a86b2b5
Salmankhansallu <78694695+Salmankhansallu@users.noreply.github.com>
2022-07-05 18:16:52 +0530

    Updates

---
+++ precache-manifest.78f49987aa3b163e1d7b3de03f0404b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5a78fa9a05fa8e77c0e",
+    "url": "/react-app/static/css/main.5fa148d3.chunk.css"
+  },
+  {
+    "revision": "e5a78fa9a05fa8e77c0e",
+    "url": "/react-app/static/js/main.ddffac09.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "807107cc503a52653cf2",
+    "url": "/react-app/static/css/2.1611856a.chunk.css"
+  },
+  {
+    "revision": "807107cc503a52653cf2",
+    "url": "/react-app/static/js/2.d4f0e911.chunk.js"
+  },
+  {
+    "revision": "51ae39ec63f92b03826b7b55ad948252",
+    "url": "/react-app/index.html"
+  }
+];
