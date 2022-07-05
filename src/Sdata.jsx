142a99acfa9c459e934aa592feb0948efea7cfe4
Salmankhansallu <78694695+Salmankhansallu@users.noreply.github.com>
2022-07-05 19:26:55 +0530

    image commit

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,14 +1,14 @@
 
 const Sdata=[
     {
-        imgsrc:'image/download (1).jpg',
+        imgsrc:'image/download%20(1).jpg',
         
         title:'MMMUT',
         content:'Madan Mohan Malaviya University of Technology (MMMUT), Gorakhpur was established in 2013 and is recognised by the UGC.',
         abc:'http://www.mmmut.ac.in/'
     },
     {
-        imgsrc:'image/download (2).jpg',
+        imgsrc:'image/download%20(2).jpg',
         title:'HOSTEL',
         content:'Hostel Of The MMMUT is Best Because All Facility is available For Every Time And Ground Of Hostel is So Large.',
         abc:'http://www.mmmut.ac.in/Hostel'
@@ -20,19 +20,19 @@ const Sdata=[
         abc:'https://images.search.yahoo.com/search/images;_ylt=Awr9ImkL98FiMscAextXNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=mmmut+campus&fr2=piv-web&type=E211US885G0&fr=mcafee'
     },
     {
-        imgsrc:'image/images (2).jpg',
+        imgsrc:'image/images%20(2).jpg',
         title:'SPORTS 2019',
         content:'All MMMUT Student Participate in The Aayash 2019-20 And Running in The 100 metre Race.',
         abc:'https://www.facebook.com/sportsmmmut/'
     },
     {
-        imgsrc:'image/images (3).jpg',
+        imgsrc:'image/images%20(3).jpg',
         title:'ABHYUDHAY',
         content:'All MMMUT Student Participate in Abhyuday 2019-20 And Give Best Performance in The Dance Society Club.',
         abc:'https://www.facebook.com/abhyudaya.mmmut/'
     },
     {
-        imgsrc:'image/images (4).jpg',
+        imgsrc:'image/images%20(4).jpg',
         title:'VICE CHANCELLOR',
         content:'Present Vice Chancellor Of Madan Mohan Malviya University Of Technology,Gorakhpur.',
         abc:'https://www.amarujala.com/gorakhpur/professor-jp-pandey-becomes-new-vice-chancellor-of-mmmut#:~:text=Professor%20Jp%20Pandey%20Becomes%20New%20Vice-chancellor%20Of%20Mmmut,Updated%20Thu%2C%2023%20Jul%202020%2002%3A27%20PM%20IST'
