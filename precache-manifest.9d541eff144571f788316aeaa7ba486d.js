473f80d62869504af0a3b649411e0bea170887e2
Salmankhansallu <78694695+Salmankhansallu@users.noreply.github.com>
2022-07-05 19:28:36 +0530

    Updates

--- precache-manifest.9d541eff144571f788316aeaa7ba486d.js
+++ precache-manifest.9d541eff144571f788316aeaa7ba486d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "cb8fc2698796e87176f3",
+    "revision": "45c8c2f4c026f0c8ccc9",
     "url": "/react-app/static/css/main.5fa148d3.chunk.css"
   },
   {
-    "revision": "cb8fc2698796e87176f3",
-    "url": "/react-app/static/js/main.3684b0f2.chunk.js"
+    "revision": "45c8c2f4c026f0c8ccc9",
+    "url": "/react-app/static/js/main.bbc081be.chunk.js"
   },
   {
     "revision": "9ed022644c6f38b380ac",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-app/static/js/2.d4f0e911.chunk.js"
   },
   {
-    "revision": "822acfad9ea08259b9d29bdf35a66f4a",
+    "revision": "2a03f143d499e35283f5474f7e42392c",
     "url": "/react-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.78745a97f3891f6a5fcdc686f42c9660.js"
+  "/react-app/precache-manifest.9d541eff144571f788316aeaa7ba486d.js"
 );
 
 workbox.clientsClaim();
